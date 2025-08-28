82c160cccf742ec8cd1241074d0509842d21689b
nirvaya <nirvayathapa22@gmail.com>
2025-08-28 17:17:47 +0545

    Minor update - 0.0.1

--- PKM.py
+++ PKM.py
@@ -2,6 +2,7 @@
 import os
 from datetime import date
 import sqlite3
+from tabulate import tabulate
 
 class note:
     def __init__(self,title,author,date,tag,content):
@@ -16,14 +17,14 @@ class note:
         print("comming soon")
 
 def add_note(notebook_name):
-    title=input("Title: ").strip()
+    title=input("Title: ").strip().capitalize()
     author=input("Author: ").strip()
     date=input("Date: ").strip()
     tags=input("Tag:")
     content=input("Content: ")
     new_note=note(title,author,date,tags.split(" "),content)
     file_path=f"Notebooks/{notebook_name}/{title}.md"
-    with open (file_path, "a+") as fopen_note:
+    with open (file_path, "w+") as fopen_note:
         fopen_note.write(f"# Title: {new_note.title}\n")
         fopen_note.write(f"## Author: {new_note.author}\n")
         fopen_note.write(f"### Date: {new_note.date}\n")
@@ -31,13 +32,44 @@ def add_note(notebook_name):
         fopen_note.write(f"*{new_note.content}*")
     insert_into_database(new_note.title,new_note.author,new_note.date,new_note.tag,notebook_name)
 
-def list_note():
-    print("Soon coming")
+def list_note(notebook_name):
+    with sqlite3.connect("Database/main_database.db") as connection:
+        cursor=connection.cursor()
+        list_notes=f'''
+        SELECT * from Notes WHERE Notebook='{notebook_name}';
+        '''
+        cursor.execute(list_notes)
+        output=cursor.fetchall()
+        print(tabulate(output,headers=['Code','Title','Author','Date','Tags','Notebook'],tablefmt="grid"))
 
 
+def view_note(notebook_name):
+    header=['Code','Title','Author','Date','Tags','Notebook']
+    with sqlite3.connect("Database/main_database.db") as connection:
+        cursor=connection.cursor()
+        list_notes=f'''
+        SELECT * from Notes WHERE Notebook='{notebook_name}';
+        '''
+        cursor.execute(list_notes)
+        output=cursor.fetchall()
+        print(tabulate(output,headers=header,tablefmt="grid"))
+        
+    ask_code_no=int(input("Enter code to view: "))
+    for ele in output:
+        _no_ , _title_,*_info_,_notebook_ = ele
+        if _no_ == ask_code_no:
+            with open(f"Notebooks/{_notebook_}/{_title_}.md" ,"r") as fread:
+                print(fread.read())
+            break
+            
+    
 
-def view_note():
-    print("coming soon")
+
+def filter_note():
+    pass
+
+def search_by_keyword():
+    pass
 
 
 def insert_into_database(Title,Author,Date,Tags,Notebook_name):
@@ -46,7 +78,7 @@ def insert_into_database(Title,Author,Date,Tags,Notebook_name):
         cursor=connection.cursor()
         create_table='''
         CREATE TABLE IF NOT EXISTS Notes(
-            Id INTEGER  PRIMARY KEY AUTOINCREMENT,
+            Code INTEGER  PRIMARY KEY AUTOINCREMENT,
             Title TEXT,
             Author TEXT,
             Date TEXT,
@@ -72,7 +104,8 @@ class notebook:
     def __call__(self):
         return self.name
     def addnote(self):
-        add_note()
+        # add_note(self.name)
+        pass
     def listnote(self):
         list_note()
     def viewnote(self):
@@ -81,9 +114,10 @@ class notebook:
         pass
     def searchbykeywords(self):
         pass
-    
+
+
+
 def ask_user():
-    # os.getcwd()
     main_prompt=input("1.Add notebook\n2.Open existing notebook\n3.Filter by tags\nEnter your option: ")
     if main_prompt.strip() == "1":
         notebook_name=input("Enter your notebook name: ").strip().capitalize()
@@ -104,10 +138,13 @@ def ask_user():
             ask_about_notebook=input("[1] Add new note\n[2] List notes\n[3] View notes\n[4] Search by keywords\n[5] Filter by tags\nEnter your choice: ").strip()
             if ask_about_notebook == "1":
                 add_note(ask_notebook_name)
+                # ask_notebook_name.addnote()
             elif ask_about_notebook == "2":
-                pass #List notes
+                list_note(ask_notebook_name) #List notes
+
             elif ask_about_notebook =="3":
-                pass #view notes
+
+                view_note(ask_notebook_name) #view notes
             elif ask_about_notebook == "4":
                 pass #search by keywords
             elif ask_about_notebook =="5":
